d2f4650bb57ef3369a8df61ad2969403de26184d
max-spark <max@sparkmicrogrants.org>
2017-12-01 12:17:05 -0700

    add new pingdom

--- static/src/js/pingdom.js
+++
@@ -1,9 +0,0 @@
-var _prum = [['id', '5a1423a253eca63d53edbf98'],
-             ['mark', 'firstbyte', (new Date()).getTime()]];
-(function() {
-    var s = document.getElementsByTagName('script')[0]
-      , p = document.createElement('script');
-    p.async = 'async';
-    p.src = '//rum-static.pingdom.net/prum.min.js';
-    s.parentNode.insertBefore(p, s);
-})();
